1ab7a67b2638ac02ea878f4ef392863eb8744ba7
Patrik-03 <patrik.drdak007@gmail.com>
2024-03-16 23:00:19 +0100

    almost sign in

--- QuickBox/endpoints/ws.py
+++ QuickBox/endpoints/ws.py
@@ -60,42 +60,21 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             message = await websocket.receive_text()
-            message = json.loads(message)  # Parse the JSON message
-            message_type = message.get('type')
-            data = message.get('data')
-            data = json.loads(data)
+            print(message)
+            data = json.loads(message)
             data_type = data.get('type')
             data_content = data.get('content')
-            if message_type == 'GET':
-                if data_type == 'signInEmail':
-                    message = getUserEmail(data_content)
-                    if message:
-                        result = {'result': 'true'}
-                        await websocket.send_text(json.dumps(result))
-                    else:
-                        result = {'result': 'false'}
-                        await websocket.send_text(json.dumps(result))
-                elif data_type == 'signInPassword':
-                    message = getUserPassword(data_content)
-                    if message:
-                        result = {'result': 'true'}
-                        await websocket.send_text(json.dumps(result))
-                    else:
-                        await websocket.send_text(json.dumps({'result': 'false'}))
-
-            elif message_type == 'POST':
-                message = {'error': 'Invalid message type'}
-                await websocket.send_text(json.dumps(message))
-
-            elif message_type == 'PUT':
-                message = {'error': 'Invalid message type'}
-                await websocket.send_text(json.dumps(message))
-
-            elif message_type == 'DELETE':
-                message = {'error': 'Invalid message type'}
-                await websocket.send_text(json.dumps(message))
-
-            else:
-                message = {'error': 'Invalid message type'}
+            if data_type == 'signInEmail':
+                message = getUserEmail(data_content)
+                if message:
+                    await websocket.send_text(json.dumps({'type': data_type, 'content': 'true'}))
+                else:
+                    await websocket.send_text(json.dumps({'type': data_type, 'content': 'false'}))
+            elif data_type == 'signInPassword':
+                message = getUserPassword(data_content)
+                if message:
+                    await websocket.send_text(json.dumps({'type': data_type, 'data': 'true'}))
+                else:
+                    await websocket.send_text(json.dumps({'type': data_type, 'data': 'false'}))
     except ConnectionClosedError:
         await websocket.close()
